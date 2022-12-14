d2984193ff732707690a6540d34733483a99d158
tahirafahradovaaa <tairafahrad@icloud.com>
2022-12-14 22:43:20 +0400

    post request, console tasks

--- js/console.js
+++ js/console.js
@@ -9,7 +9,9 @@ let price = [];
 function average() {
   network.getAll("/products").then((data) => {
     data.forEach((element) => {
-      price.push(element.unitsInStock);
+      if (element.unitsInStock !== undefined) {
+        price.push(element.unitsInStock);
+      }
     });
     calc(price);
   });
@@ -33,13 +35,83 @@ let customers = [];
 function country() {
   network.getAll("/customers").then((data) => {
     let sorted = data.filter((q) => q.address.city === "London");
+    console.log(data[0]);
     sorted.forEach((element) => {
-      customers.push(element.contactName);
+      if (element.address.city !== undefined) {
+        customers.push(element.contactName);
+      }
     });
     console.log("This customers are from London ", customers);
+  });
+}
+
+function yearly() {
+  network.getAll("/orders").then((data) => {
+    const dataArr = data.sort((a, b) => b.orderDate - a.orderDate);
+    dataArr.forEach((element) => {
+      let targetYear = new Date();
+      let targetYear2 = new Date();
+      targetYear.setFullYear(1997);
+      targetYear2.setFullYear(1996);
+      let d1 = new Date(element.orderDate);
+      if (
+        d1.getFullYear === targetYear.getFullYear ||
+        d1.getFullYear === targetYear2.getFullYear
+      ) {
+        console.log(element);
+      }
+    });
   });
 }
 getData();
 average();
 letter();
 country();
+// yearly();
+
+function myBeloved() {
+  network.getAll("/orders").then((data) => {
+    let customersOrderData = [];
+    data.forEach((element) => {
+      let sum = 0;
+      element.details.forEach((item) => {
+        sum += item.unitPrice * item.quantity * (1 - item.discount);
+      });
+      element.total = sum;
+      let customer = customersOrderData.find(
+        (q) => q.customerId == element.customerId
+      );
+      if (!customer) {
+        let addedData = {
+          customerId: element.customerId,
+          customerTotalAmount: element.total,
+        };
+
+        customersOrderData.push(addedData);
+      } else {
+        customer.customerTotalAmount =
+          customer.customerTotalAmount + element.total;
+      }
+    });
+    let sortedCustomer = customersOrderData.sort(
+      (a, b) => a.customerTotalAmount - b.customerTotalAmount
+    );
+    console.log(sortedCustomer);
+    console.log(
+      sortedCustomer[sortedCustomer.length - 1].customerId +
+        "is the beloved customer. And bought " +
+        sortedCustomer[sortedCustomer.length - 1].customerTotalAmount.toFixed(
+          2
+        ) +
+        " worth products"
+    );
+    console.log(
+      sortedCustomer[0].customerId +
+        "is the most hated customer. And bought " +
+        sortedCustomer[0].customerTotalAmount.toFixed(2) +
+        " worth products"
+    );
+  });
+}
+
+myBeloved();
--- js/pageThree.js
+++ js/pageThree.js
@@ -1,4 +1,10 @@
 const tbody = document.querySelector("tbody");
+const modalBtn = document.querySelector(".input-update");
+const companyInput = document.querySelector(".nameInput");
+const contactInput = document.querySelector(".contactInput");
+const putId = [];
+
+const modal = document.querySelector(".modal");
 function getData() {
   network.getAll("/customers").then((data) => {
     const dataArr = data.sort((a, b) =>
@@ -14,22 +20,31 @@ function fill(data) {
   tbody.innerHTML = "";
   data.forEach((element) => {
     const id = element.id;
-
+    let companyName = element.companyName;
+    let contactName = element.contactName;
     const newTr = document.createElement("tr");
     const newBtn = document.createElement("button");
+    const updateBtn = document.createElement("button");
+    updateBtn.innerHTML = "UPDATE";
+    updateBtn.addEventListener("click", () => {
+      modal.style.display = "flex";
+      companyInput.value = companyName;
+      contactInput.value = contactName;
+      putId.push(id);
+    });
     newBtn.setAttribute("value", id);
     newBtn.classList.add("delete");
     newBtn.innerHTML = "DELETE";
     const companyTd = document.createElement("td");
     const contactTd = document.createElement("td");
-    let companyName = element.companyName;
-    let contactName = element.contactName;
+
     companyTd.innerText = companyName;
     contactTd.innerText = contactName;
     let idName = "";
     newTr.appendChild(companyTd);
     newTr.appendChild(contactTd);
     newTr.appendChild(newBtn);
+    newTr.appendChild(updateBtn);
     tbody.appendChild(newTr);
     newBtn.addEventListener("click", (e) => {
       idName = e.target.value;
@@ -41,4 +56,39 @@ function fill(data) {
 
 const deleteData = async (id) => {
   network.delete("/customers", id).then((data) => {});
+};
+
+// const update = () => {
+
+//   let companyName = nameInput.id;
+//   let contactName = nameInput.value;
+//   axios.put(`https://northwind.vercel.app/api/products/${id}`, {
+//       companyName: companyName,
+//       categoryId: categoryId
+//   })
+//       .then(res => {
+
+//           getAllProductsByCategoryId(categoryId)
+//               .then(data => {
+//                   liLoad(data);
+//               })
+
+//       })
+// }
+
+modalBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+  update(companyInput.value, contactInput.value, putId);
+});
+
+const update = (compName, contName, id) => {
+  axios
+    .put(`https://northwind.vercel.app/api/customers/${id}`, {
+      companyName: compName,
+      contactName: contName,
+      id: id,
+    })
+    .then((res) => {
+      getData();
+    });
 };
